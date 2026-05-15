7766d0c4a5bd674b579682d58a474b022dd56b06
Zeyad <ziadm4772@gmail.com>
2026-05-15 11:44:34 +0300

    Initial Commit

---
+++ SomaTracker/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SomaTracker
+//
+//  Created by Zeyad Hussein on 15/05/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
